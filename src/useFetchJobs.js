97b7abb96047ecc8526888ae4399440fda071eee
lasha tatulashvili <lashatatu@gmail.com>
2020-07-25 15:05:04 +0400

    initial comit

---
+++ src/useFetchJobs.js
@@ -0,0 +1,42 @@
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return {loading: true, jobs: []}
+        case ACTIONS.GET_DATA:
+            return {...state, loading: false, jobs: action.payload.jobs}
+        case ACTIONS.ERROR:
+            return {...state, loading: false, error: action.payload, jobs: []}
+        default:
+            return state
+    }
+
+}
+
+export default function useFetchJobs(params, page) {
+    const [state, dispatch] = useReducer(params, {jobs: [], loading: true})
+
+    useEffect(() => {
+        dispatch({type: ACTIONS.MAKE_REQUEST})
+        axios.get(BASE_URL, {
+            params: {markdown: true, page: page, ...params}.than(res => {
+                dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
+            })
+        }).catch(e=>{
+            dispatch({type: ACTIONS.GET_DATA, payload: {error: e}})
+        })
+    }, [params, page])
+
+    dispatch({type: 'hello', payload: {x: 3}})
+    return state
+}
